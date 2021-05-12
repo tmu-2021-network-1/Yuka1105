fab6bce567b2d7c80639a57abf2e22d2cb4b996f
Yuka1105 <56023851+Yuka1105@users.noreply.github.com>
2021-05-12 23:35:12 +0900

    IAサイトAPI宿題

--- docs/script.js
+++ docs/script.js
@@ -7,18 +7,58 @@ const renderJson = (json) => {
   const studios = json.records;
   
   studios.forEach(studio => {
+
+   const buttonDiv = document.createElement('div');
+   buttonDiv.className ='button-div';
+
+   const button = document.createElement("a");
+   button.className = 'studio-button'
+   button.href = `#${studio['name-ja']}`
+   button.textContent = studio['name-ja'].slice( 0, -4 ) ;
+
+
    const studioDiv = document.createElement('div');
+   studioDiv.className ='studio-div';
+
+   const  studioImage = document.createElement('img');
+   studioImage.id =studio['name-ja'];
+   studioImage.className = 'studio-image';
+   studioImage.src = studio['photo1'];
+   studioImage.alt = 'スタジオの画像です。';
+
+   const studioDivTwo = document.createElement('div');
+   studioDivTwo.className ='studio-div-two';
+
+   const studioDivThree = document.createElement('div');
+   studioDivThree.className ='studio-div-three';
+
    const studioTitle = document.createElement("span");
    studioTitle.className = 'studio-title';
    studioTitle.textContent = studio['name-ja'];
-   const studioTitleEn = document.createElement("span");
-   studioTitleEn.className = 'studio-title-en';
-   studioTitleEn.textContent = studio['name-en'];
-   studioDiv.appendChild(studioTitle);
-   studioDiv.appendChild(studioTitleEn);
+
+   const kindOfCore = document.createElement("span");
+   kindOfCore.className = 'kind-of-core';
+   kindOfCore.textContent = `#${studio['core-ja']}`;
+
+   const detail = document.createElement("a");
+   detail.className = 'read-more'
+   detail.textContent = 'Read more';
+   
+
+   studioDivTwo.appendChild(studioTitle);
+   studioDivTwo.appendChild(kindOfCore);
+
+   studioDivThree.appendChild(studioDivTwo);
+   studioDivThree.appendChild(detail);
+
+   buttonDiv.appendChild(button);
+   studioDiv.appendChild(studioImage);
+   studioDiv.appendChild(studioDivThree);
+   
+   document.getElementById('buttons').appendChild(buttonDiv);
    document.getElementById('studios').appendChild(studioDiv);
  });
-  document.getElementById('result').textContent = JSON.stringify(json, null, 2);
+  /*document.getElementById('result').textContent = JSON.stringify(json, null, 2);*/
 }
 
 const getData = async () => {
